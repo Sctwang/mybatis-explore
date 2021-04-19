e0532e56d22df41e74675dc2114f154a8e2e538c
sctwang <1286185556@qq.com>
2021-04-19 20:31:17 +0800

    Add LeetCode283

---
+++ mybatis-3-mybatis-3.5.6/src/main/java/top/jonas/mybatis/leetcode/LeetCode283.java
@@ -0,0 +1,39 @@
+package top.jonas.mybatis.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author wyz
+ * @date 2021/4/19 20:22
+ * <p>
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class LeetCode283 {
+
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                j++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        new LeetCode283().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
